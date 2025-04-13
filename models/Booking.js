4cf2ba71f3cbfc9498abab4200d86d965bd3713e
Sayan <144345899+iamsayan28@users.noreply.github.com>
2025-04-13 16:03:06 +0530

    Update Booking.js

--- models/Booking.js
+++ models/Booking.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true 
+    },
+    userId: String,
+    movieId: String,
+    seats: Number
+});
+
+module.exports = mongoose.models.Booking || mongoose.model('Booking', bookingSchema);
 
